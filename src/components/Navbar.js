1b888c26b725a1586ef6f445656e1ad70136484c
AlejandroFarel62 <32970185+Martin444@users.noreply.github.com>
2019-05-27 23:56:09 -0300

    PWA casi habilitado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from '../images/logo.svg'
+import logo from '../images/logo.png'
 import {Link} from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-sm navbar-light bg-ligth">
             <Link to="/" className="navbar-brand">
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" style={{maxHeight: "30px"}} className="brand" />
             </Link>
             <div className="collapse navbar-collapse show ml-sm-5">
                 <ul className="navbar-nav">
